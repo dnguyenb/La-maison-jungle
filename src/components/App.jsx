8267a5347b4a8216bfc67f1b3ec113d3e1110069
David Nguyen Blot <d.nguyenblot@gmail.com>
2023-12-31 23:48:19 +0100

    ajout Footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
 import Banner from './Banner';
 import ShoppingList from './ShoppingList';
+import Footer from './Footer';
 // import Cart from './Cart';
 
 function App() {
 	return (
 		<div>
 			<Banner />
-			{/* <Cart /> */} 
+			{/* <Cart /> */}
 			<ShoppingList />
+			<Footer />
 		</div>
 	);
 }
