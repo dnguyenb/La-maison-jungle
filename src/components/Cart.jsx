6e0a08c54ddd1335e3990d56ee776a12c439dcf4
David Nguyen Blot <d.nguyenblot@gmail.com>
2024-01-03 09:21:56 +0100

    partage state local entre Cart et ShoppingList via App

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,25 +1,32 @@
 import { useState } from 'react';
 import '../styles/Cart.css';
 
-function Cart() {
-
-	const [cart, updateCart] = useState(0);
+function Cart({ cart, updateCart }) {
 	const [isOpen, setIsOpen] = useState(false);
-
 	const monsteraPrice = 8;
 
 	return isOpen ? (
 		<div className="lmj-cart">
-			<button onClick={()=> setIsOpen(false)}>fermer le panier</button>
+			<button
+				onClick={() => setIsOpen(false)}
+				className="lmj-cart-toggle-button"
+			>
+				fermer le panier
+			</button>
 			<h2>Panier</h2>
-			<div>
-				Monstera : {monsteraPrice}€
-				<button onClick={() => updateCart(cart + 1)}>Ajouter au panier</button>
 			<h3>Total : {monsteraPrice * cart}€</h3>
-				<button onClick={()=> updateCart(0)}>Vider le panier</button>
-			</div>
+			<button onClick={() => updateCart(0)}>Vider le panier</button>
 		</div>
-	) : <button onClick={()=> setIsOpen(true)}>Ouvrir le panier</button>;
+	) : (
+		<div className="lmj-cart-closed">
+			<button
+				onClick={() => setIsOpen(true)}
+				className="lmj-cart-toggle-button"
+			>
+				Ouvrir le panier
+			</button>
+		</div>
+	);
 }
 
 export default Cart;
--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -2,7 +2,7 @@ import { plantList } from '../datas/plantList';
 import '../styles/ShoppingList.css';
 import PlantItem from './PlantItem';
 
-function ShoppingList() {
+function ShoppingList({ cart, updateCart }) {
 	const categories = plantList.reduce(
 		(acc, plant) =>
 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
@@ -10,7 +10,7 @@ function ShoppingList() {
 	);
 
 	return (
-		<div>
+		<div className="lmj-shopping-list">
 			<ul>
 				{categories.map((cat) => (
 					<li key={cat}>{cat}</li>
@@ -19,13 +19,12 @@ function ShoppingList() {
 
 			<ul className="lmj-plant-list">
 				{plantList.map(({ id, cover, name, water, light }) => (
-					<PlantItem
-						id={id}
-						cover={cover}
-						name={name}
-						water={water}
-						light={light}
-					/>
+					<div key={id}>
+						<PlantItem cover={cover} name={name} water={water} light={light} />
+						<button onClick={() => updateCart(cart + 1)}>
+							Ajouter au panier
+						</button>
+					</div>
 				))}
 			</ul>
 		</div>
